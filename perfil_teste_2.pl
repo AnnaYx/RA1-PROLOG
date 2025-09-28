65928c0c8da0f5b1f060e8c2821d06a450abcb7f
Nicole Guarnieri <130791306+nick11nic@users.noreply.github.com>
2025-09-28 12:04:10 -0300

    Perfil de teste 2

---
+++ perfil_teste_2.pl
@@ -0,0 +1,14 @@
+% Perfil: afinidade com Web
+resposta(matematica, n).
+resposta(logica, n).
+resposta(ia, n).
+resposta(design, s).
+resposta(frontend, s).
+resposta(backend, s).
+resposta(criptografia, n).
+resposta(redes, n).
+resposta(investigacao, n).
+resposta(programacao, n).
+resposta(dados, n).
+resposta(administracao, n).
+resposta(problemas, s).
